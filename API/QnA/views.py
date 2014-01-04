a40c379fd9a0482ecf3d2abe2ee8c1651ec924ab
Henri Hänninen <henri.hanninen@live.com>
2014-01-04 17:41:15 +0200

    Added post method to UserAPI.

--- API/QnA/views.py
+++ API/QnA/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from QnA.models import User
+import json
 
 # Create your views here...
 
@@ -13,3 +14,16 @@ class UserAPI(APIView):
         for user in userdata:
             data.append(user.serialize())
         return Response({"users": data}, 200)
+
+    #VALIDATE
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+        user_id = data['user_id']
+        reputation = data['reputation']
+        user = User()
+        user.username = username
+        user.user_id = user_id
+        user.reputation = reputation
+        user.save()
+        return Response(200)
