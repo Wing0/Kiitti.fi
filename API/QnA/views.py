d4940f2f646ff90726b8b04106d0f07bb7450cc5
Henri Hänninen <henri.hanninen@live.com>
2014-01-04 15:39:14 +0200

    Converted tabs to spaces

--- API/QnA/views.py
+++ API/QnA/views.py
@@ -3,13 +3,13 @@ from django.http import HttpResponse
 from rest_framework.views import APIView
 from QnA.models import User
 
-# Create your views here.
+# Create your views here...
 
 class UserAPI(APIView):
 
-	def get(self, request):
-		data = []
-		userdata = User.objects.all()
-		for user in userdata:
-			data.append(user.serialize())
-		return Response("users": data, 200)
+    def get(self, request):
+        data = []
+        userdata = User.objects.all()
+        for user in userdata:
+            data.append(user.serialize())
+        return Response("users": data, 200)
