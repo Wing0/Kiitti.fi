eb3cfa8896ce8187dad5ed3c826c47e4969e69c3
Oskari Vinko <oskari.vinko@gmail.com>
2014-01-05 16:53:05 +0200

    Catch non-unique username when saving, code enhancements

--- API/QnA/views.py
+++ API/QnA/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
+from django.db import IntegrityError
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from QnA.models import User, Vote, AbstractMessage, Comment, Answer
 import json
-import re
 
 # Create your views here...
 
@@ -57,12 +57,35 @@ class UserAPI(APIView):
     #VALIDATE
     def post(self, request):
         data = json.loads(request.body)
+        if ("username" in data
+            and "email" in data
+            and "firstName" in data
+            and "lastName" in data
+            and "organizationId" in data
+            and "password" in data):
 
-        user = User(username=data["username"], email=data["email"], first_name=data["firstName"], last_name=data["lastName"], organization_id=data["organizationId"])
-        valid, messages = user.validate()
-        if valid:
-            user.save()
-        return Response({"messages":messages, "valid":valid},200)
+            user = User(username=data["username"],
+                        email=data["email"],
+                        first_name=data["firstName"],
+                        last_name=data["lastName"],
+                        organization_id=data["organizationId"],
+                        password=data["password"])
+
+            valid, messages = user.validate()
+            if valid:
+                user.set_password(user.password)
+                try :
+                    user.save()
+                except IntegrityError, e:
+                    valid = False
+                    if e.message == "column username is not unique":
+                        messages.append({"type":"alert", "content": "Username already in use.", "identifier":"username"})
+                    else:
+                        messages.append({"type":"alert", "content": e.message, "identifier":""})
+        else:
+            messages = [{"type":"Alert","content":"Something is missing","identifier":""}]
+            valid = False
+        return Response({"messages":messages},200)
 
 class VoteAPI(APIView):
 
