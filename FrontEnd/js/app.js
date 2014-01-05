1bb631028a86c0c42ccadb0cbbfb6850913ff129
Klaus Nygård <klaus.nygard@gmail.com>
2014-01-05 22:27:13 +0200

    AngularJS + CSS

--- FrontEnd/js/app.js
+++ FrontEnd/js/app.js
@@ -56,6 +56,24 @@ app.controller('BrowsePopularController', function($scope, QuestionFactory) {
 
 });
 
+app.controller('SubmitAnswerController', function($scope, QuestionFactory) {
+
+  var answertest = {
+    "id": 2,
+    "content": "Uuskommenti asglkjgsdlakjgs",
+    "user_id": 125,
+    "username": "Zorro",
+    "votes_up": 12,
+    "votes_down": 0,
+    "date": "2014-01-02T14:00:00.000Z"
+  }
+
+  $scope.send = function() {
+    console.log("User: ", $scope.user.name, "Message: ", $scope.message);
+    $scope.question.answers.push(answertest);
+  };
+});
+
 /* DIRECTIVES */
 
 app.directive('qnaComment', function() {
