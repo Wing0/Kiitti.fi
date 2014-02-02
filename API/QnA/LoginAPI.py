35ab6c37f62ea1e5a63d94eb8b71319c2d664ed8
Oskari Vinko <oskari.vinko@gmail.com>
2014-02-02 14:03:06 +0200

    LoginAPI docstrings

--- API/QnA/LoginAPI.py
+++ API/QnA/LoginAPI.py
@@ -12,6 +12,36 @@ class LoginAPI(APIView):
 
     @method_decorator(ensure_csrf_cookie)
     def post(self, request):
+        '''
+        Log the user in using an username and password
+
+        @return:
+            200: Logging in was successful or user was already logged in
+                list of appropriate error messages if already logged in
+                @example:
+                    {
+                        "messages":[{"content":"An example error message.","identifier":"example"}]
+                    }
+
+            403: User does not have permission to log in (banned)
+                list of appropriate error messages
+                example:{
+                            "messages":[{"content":"An example error message.","identifier":"example"}]
+                        }
+
+            404: User was not found
+                list of appropriate error messages
+                example:{
+                            "messages":[{"content":"An example error message.","identifier":"example"}]
+                        }
+
+            400: Invalid request, parameters were missing or wrong type
+                list of appropriate error messages
+                example:{
+                            "messages":[{"content":"An example error message.","identifier":"example"}]
+                        }
+
+        '''
         if request.user.is_authenticated():
             return Response({"messages":[{"content":"User was already logged in.","identifier":""}]},200)
 
@@ -28,12 +58,37 @@ class LoginAPI(APIView):
                     return Response({"messages":[{"content":"User is banned.","identifier":""}]},403)
             else:
                 return Response({"messages":[{"content":"Username and/or password was incorrect.","identifier":""}]},404)
-        return Response({"messages":[{"content":"Username and/or password was missing.","identifier":""}]}, 404)
+        return Response({"messages":[{"content":"Username and/or password was missing.","identifier":""}]}, 400)
 
 
     # GetCsrfToken
     @method_decorator(ensure_csrf_cookie)
     def get(self, request):
+        '''
+        Check if user is logged in or not
+
+        @return:
+            200: Request was successful, user is logged in
+                return user object serlialization
+                @example:
+                    {
+                        "user":{
+                                  "username":"test",
+                                  "password":"test",
+                                  "organizationId":1,
+                                  "email":"test@test.test",
+                                  "firstName":"test",
+                                  "lastName":"test"
+                                }
+                    }
+
+            401: Unauthorized, the user has to be logged in to perform this action
+                list of appropriate error messages
+                example:{
+                            "messages":[{"content":"An example error message.","identifier":"example"}]
+                        }
+
+        '''
         if request.user.is_authenticated():
             return Response({"user":request.user.serialize()},200)
         else:
