83ff78dc6a2946b0f56a4549aef70296ae67a285
Oskari Vinko <oskari.vinko@gmail.com>
2014-02-02 20:09:34 +0200

    Move exclude_old_version from view_utils to utils

--- API/QnA/utils.py
+++ API/QnA/utils.py
@@ -3,3 +3,16 @@ def compose_message(content, identifier=""):
 
 def create_message(content, identifier=""):
     return {"messages": [compose_message(content, identifier)]}
+
+def exclude_old_versions(message_list):
+    # Sort all messages by version
+    message_list.sort(key=lambda q: -q.version)
+
+    used = []
+    messages = []
+    # Append first occurrence of message id to messages
+    for q in message_list:
+        if q.message_id not in used:
+            messages.append(q)
+            used.append(q.message_id)
+    return messages
