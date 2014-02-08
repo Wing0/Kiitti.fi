51f6c844d569e2baf7505a8f8970189439c64efe
Oskari Vinko <oskari.vinko@gmail.com>
2014-02-08 10:06:59 +0200

    in QuestionAPI abs_data parameter changed to q for clarity

--- API/QnA/QuestionAPI.py
+++ API/QnA/QuestionAPI.py
@@ -49,13 +49,13 @@ class QuestionAPI(APIView):
             messages.append(compose_message("User must be logged in.", "user"))
             return Response({"messages":messages}, 401)
 
-        abs_data = post_abstract_message(Question(), data)
+        q = post_abstract_message(Question(), data)
 
         title = data.get('title')
-        abs_data.title = title
-        abs_data.organization = request.user.organization
-        abs_data.user = request.user
-        messages = abs_data.validate()
+        q.title = title
+        q.organization = request.user.organization
+        q.user = request.user
+        messages = q.validate()
         '''
         # save tags functionality. This should be part of modify question post() command
         # Similar functionality should be implemented when question is created
@@ -63,7 +63,7 @@ class QuestionAPI(APIView):
             # Save tags
             taglist = data.get("tags")
             if taglist and isinstance(taglist,list):
-                existing_tags = [tag_entry.tag.name for tag_entry in TagEntry.objects.filter(message_id=abs_data.message_id)]
+                existing_tags = [tag_entry.tag.name for tag_entry in TagEntry.objects.filter(message_id=q.message_id)]
                 tagnames = []
                 for tag in list(set(taglist)): #List(set()) thing removes duplicates
                     if tag not in existing_tags:
@@ -71,16 +71,16 @@ class QuestionAPI(APIView):
                 for tagname in tagnames:
                     #try:
                     tag = Tag.objects.get(name=tagname)
-                    entry = TagEntry(tag=tag, message_id=abs_data.message_id, creator=abs_data.user)
+                    entry = TagEntry(tag=tag, message_id=q.message_id, creator=q.user)
                     entry.save()
                     #except:
                     #    messages.append(compose_message("Tag %s was not found." % tagname, "tags"))
                     '''
         if len(messages) == 0:
-            if abs_data.message_id:
-                abs_data.save_changes()
+            if q.message_id:
+                q.save_changes()
             else:
-                abs_data.save()
+                q.save()
             return Response({"messages": messages}, 201)
 
         return Response({"messages":messages},400)
