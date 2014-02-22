018202826a5d7a900e1bc07da38dce1fba9fc177
Klaus Nygård <klaus.nygard@gmail.com>
2014-02-22 20:17:32 +0200

    initial authentication test

--- API/QnA/tests/tests_authentication.py
+++ API/QnA/tests/tests_authentication.py
@@ -1,5 +1,7 @@
 from rest_framework.test import APITestCase
-from rest_framework.test import APIRequestFactory
+from rest_framework.test import APIClient
+from rest_framework.authtoken.models import Token
+
 from rest_framework import status
 
 from QnA.API.LoginAPI import LoginAPI
@@ -7,15 +9,26 @@ from QnA.models import User, Organization
 
 
 class AuthenticationTest(APITestCase):
+
     def setUp(self):
-        self.factory = APIRequestFactory()
-        self.organization = Organization(organization_id=1, name="testorganization")
+        self.client = APIClient()
+        self.organization = Organization(
+            organization_id=1, name="testorganization")
         self.user = User.objects.create_user(
             username='test', email='test@test.test', password='test', organization=self.organization)
 
     def test_login(self):
-        request = self.factory.get('/auth/login', {"username": "test", "password": "test"})
+        request = self.client.post(
+            '/auth/login', {"username": "test", "password": "test"})
+        token = request.data.get('token', None)
+        self.assertIsNotNone(
+            token, "Could not receive authentication token on login post.")
+        self.assertEqual(request.status_code, 200,
+                         "Status code on /auth/login was not 200.")
 
-        view = LoginAPI.as_view()
-        response = view(request)
-        self.assertEqual(response.status_code, 200)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+        request = self.client.get('/auth/load')
+        self.assertEqual(request.status_code, 200,
+                         "Status code on /auth/load was not 200.")
+        self.assertEqual(request.data['username'], self.user.username,
+                         "Usernames for loaded user and initialized user do not match.")
