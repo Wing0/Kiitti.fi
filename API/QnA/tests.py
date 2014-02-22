18f78f7e86f1950ae8e98fe9e2218e4d2cc80da9
Henri Hänninen <henri.hanninen@live.com>
2014-02-22 17:47:56 +0200

    Manager file added.

--- API/QnA/tests.py
+++ API/QnA/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from QnA.models import User
 
 # Create your tests here.
+
+class CommentTest(TestCase):
+
+
+    def testGet(self):
+        c = Client()
+        reponse = c.get("/users?id=1")
+        user = User.objects.get(user_id=1)
+        self.assertEqual(reponse.user, user)
+
+    def testSec(self):
+        self.assertEqual(1, 0)
