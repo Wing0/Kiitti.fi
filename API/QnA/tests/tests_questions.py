680a57eb61107af595faf5d42f41e65a028b53ae
Oskari Vinko <oskari.vinko@gmail.com>
2014-02-22 18:34:16 +0200

    More question test

--- API/QnA/tests/tests_questions.py
+++ API/QnA/tests/tests_questions.py
@@ -14,6 +14,12 @@ class QuestionTests(APITestCase):
     def setUp(self):
         call_command('loaddata', 'min_test_data')
         self.factory = APIRequestFactory()
+        self.example_question_data = {
+                "title": "Why does the sun go down every night?",
+                "content":"Why does the sun go down every night? I don't understand!",
+                "messageId":3,
+                "tags": ["tagFirst", "tagSecond"]
+            }
 
     def test_unauthorized_access(self):
         url = "/questions/"
@@ -23,13 +29,38 @@ class QuestionTests(APITestCase):
         response = view(request)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    def test_authorized_access(self):
-        url = "/auth/login/"
+    def test_unauthorized_post(self):
+        url = "/questions/"
+        data = self.example_question_data
+        request = self.factory.post(url, data, format='json')
+        view = QuestionAPI.as_view()
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_authorized_access_empty(self):
+        url = "/questions/"
         data = {}
         request = self.factory.post(url, data, format='json')
-        view = LoginAPI.as_view()
+        request.user = User.objects.get(username="admin")
+        view = QuestionAPI.as_view()
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_authorized_post(self):
+        url = "/questions/"
+        data = self.example_question_data
+        request = self.factory.post(url, data, format='json')
+        request.user = User.objects.get(username="admin")
+        view = QuestionAPI.as_view()
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)THORIZED)
+
+    def test_authorized_access(self):
+        url = "/questions/"
+        data = {}
+        request = self.factory.post(url, data, format='json')
+        request.user = User.objects.get(username="admin")
+        view = QuestionAPI.as_view()
         response = view(request)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_creation(self):
-        pass
