a6109240a1862849949d96bf0e5af8104633b589
Klaus Nygård <klaus.nygard@gmail.com>
2014-02-23 14:42:39 +0200

    removed unnecessary imports

--- API/QnA/API/LogoutAPI.py
+++ API/QnA/API/LogoutAPI.py
@@ -1,10 +1,6 @@
 from django.contrib.auth import logout
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import AllowAny
-from QnA.utils import *
-import json
 
 
 class LogoutAPI(APIView):
