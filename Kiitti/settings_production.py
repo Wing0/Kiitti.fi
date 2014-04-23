1c29e98613e78f1ba87b487f980c789e39eb2bfc
Klaus Nygård <klaus.nygard@gmail.com>
2014-04-23 14:27:09 +0300

    added production settings

---
+++ Kiitti/settings_production.py
@@ -0,0 +1,23 @@
+from settings import *
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+ALLOWED_HOSTS = ('.kiitti.fi',)
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'kiittidb',
+#         'USER': 'kiitti',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '', # 5432
+#     },
+# }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'production.sqlite3'),
+    }
+}
